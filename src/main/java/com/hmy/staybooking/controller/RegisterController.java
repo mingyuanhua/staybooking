ac6d1adf045f064285004fd58765035bf00d5071
mingyuanhua <m.y.hua@hotmail.com>
2023-06-20 10:56:22 -0700

    Implement Register Controller

---
+++ src/main/java/com/hmy/staybooking/controller/RegisterController.java
@@ -0,0 +1,29 @@
+package com.hmy.staybooking.controller;
+
+import com.hmy.staybooking.model.User;
+import com.hmy.staybooking.model.UserRole;
+import com.hmy.staybooking.service.RegisterService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+
+    private final RegisterService registerService;
+
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @PostMapping("/register/guest")
+    public void addGuest(@RequestBody User user) {
+        registerService.add(user, UserRole.ROLE_GUEST);
+    }
+
+    @PostMapping("/register/host")
+    public void addHost(@RequestBody User user) {
+        registerService.add(user, UserRole.ROLE_HOST);
+    }
+}
+
