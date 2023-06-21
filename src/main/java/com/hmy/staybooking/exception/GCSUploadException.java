ef8611aec4e75a439911805a1957432e7d32b96c
mingyuanhua <m.y.hua@hotmail.com>
2023-06-21 10:29:56 -0700

    Add new custom exception

---
+++ src/main/java/com/hmy/staybooking/exception/GCSUploadException.java
@@ -0,0 +1,7 @@
+package com.hmy.staybooking.exception;
+
+public class GCSUploadException extends RuntimeException {
+    public GCSUploadException(String message) {
+        super(message);
+    }
+}
