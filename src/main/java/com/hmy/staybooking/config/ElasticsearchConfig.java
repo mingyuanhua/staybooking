3948fef336b7ec98ca6c2398c312e26f8c7a77fb
mingyuanhua <m.y.hua@hotmail.com>
2023-06-24 11:02:43 -0700

    Create ElasticsearchConfig

---
+++ src/main/java/com/hmy/staybooking/config/ElasticsearchConfig.java
@@ -0,0 +1,38 @@
+package com.hmy.staybooking.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+// Add the elasticsearchClient() method to create an Elasticsearch client bean
+
+// in contrast, we didn't create MySQL client bean manually because Spring Boot can
+// help create one based on the information we put in the application.properties
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.address}")
+    private String elasticsearchAddress;
+
+    @Value("${elasticsearch.username}")
+    private String elasticsearchUsername;
+
+    @Value("${elasticsearch.password}")
+    private String elasticsearchPassword;
+
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration
+                = ClientConfiguration.builder()
+                .connectedTo(elasticsearchAddress)
+                .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
+
