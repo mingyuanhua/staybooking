adfa177e49f162349fbb8601bde078c205dfdfaf
mingyuanhua <m.y.hua@hotmail.com>
2023-06-24 10:30:29 -0700

    Update StayRepository

--- src/main/java/com/hmy/staybooking/repository/StayRepository.java
+++ src/main/java/com/hmy/staybooking/repository/StayRepository.java
@@ -9,10 +9,15 @@ import java.util.List;
 
 // common methods like save, deleteById, findById are defined in the JpaRepository
 // so we only need to define our method findByHost and findByIdAndHost
+
+// Add a new method called findByIdInAndGuestNumberGreaterThanEqual()
+// besides location, the guest number is another parameter for search
 @Repository
 public interface StayRepository extends JpaRepository<Stay, Long> {
     List<Stay> findByHost(User user);
 
     Stay findByIdAndHost(Long id, User host);
+
+    List<Stay> findByIdInAndGuestNumberGreaterThanEqual(List<Long> ids, int guestNumber);
 }
 
