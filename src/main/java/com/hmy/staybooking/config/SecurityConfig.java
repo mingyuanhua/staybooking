8ad165c2e29344a22b28869f37aba944d23c5a45
mingyuanhua <m.y.hua@hotmail.com>
2023-06-28 22:08:43 -0700

    Update Security Config

--- src/main/java/com/hmy/staybooking/config/SecurityConfig.java
+++ src/main/java/com/hmy/staybooking/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hmy.staybooking.config;
 
+import com.hmy.staybooking.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,36 +8,58 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
 // Enable Spring Security AuthenticationManager
 
 // Do encryption before saving the data for security reasons
+
+// update SecurityConfig to allow /authentication/guest and /authentication/host URLs
+// enable the JwtFilter and make stay management APIs require authentication
+
+// configure the AuthenticationManager provided by Spring Security
+// to read user/authority data from the MySQL database for authentication
+
+// configure datasource for AuthenticationManager
+
+// expose AuthenticationManager as a bean so that we can use it in authentication service
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final DataSource dataSource;
 
-    public SecurityConfig(DataSource dataSource) {
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(DataSource dataSource, JwtFilter jwtFilter) {
         this.dataSource = dataSource;
+        this.jwtFilter = jwtFilter;
     }
 
-    // update SecurityConfig to allow /authentication/guest and /authentication/host URLs
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/register/*").permitAll()
                 .antMatchers(HttpMethod.POST,"/authenticate/*").permitAll()
-                .antMatchers("/stays").permitAll()
-                .antMatchers("/stays/*").permitAll()
+                .antMatchers("/stays").hasAuthority("ROLE_HOST")
+                .antMatchers("/stays/*").hasAuthority("ROLE_HOST")
+                .antMatchers("/search").hasAuthority("ROLE_GUEST")
+                .antMatchers("/reservations").hasAuthority("ROLE_GUEST")
+                .antMatchers("/reservations/*").hasAuthority("ROLE_GUEST")
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
                 .disable();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -44,23 +67,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    // configure the AuthenticationManager provided by Spring Security
-    // to read user/authority data from the MySQL database for authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
                 .authoritiesByUsernameQuery("SELECT username, authority FROM authority WHERE username = ?");
-    }
-    // configure datasource for AuthenticationManager
 
-    // expose AuthenticationManager as a bean so that we can use it in authentication service
+    }
+
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
 }
-
-
