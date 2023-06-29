0ddafc1694c4acbb40819b176aa0fa16273a434f
mingyuanhua <m.y.hua@hotmail.com>
2023-06-28 21:11:01 -0700

    Create Reservation repository

---
+++ src/main/java/com/hmy/staybooking/repository/ReservationRepository.java
@@ -0,0 +1,22 @@
+package com.hmy.staybooking.repository;
+
+import com.hmy.staybooking.model.Reservation;
+import com.hmy.staybooking.model.Stay;
+import com.hmy.staybooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByGuest(User guest);
+
+    List<Reservation> findByStay(Stay stay);
+
+    Reservation findByIdAndGuest(Long id, User guest);
+
+    List<Reservation> findByStayAndCheckoutDateAfter(Stay stay, LocalDate date);
+}
+
