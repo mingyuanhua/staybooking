8711bd4027bb82da19edf46788b5626d49e98748
mingyuanhua <m.y.hua@hotmail.com>
2023-06-28 22:07:53 -0700

    Create Stay Repository

--- src/main/java/com/hmy/staybooking/repository/StayRepository.java
+++ src/main/java/com/hmy/staybooking/repository/StayRepository.java
@@ -14,6 +14,7 @@ import java.util.List;
 // besides location, the guest number is another parameter for search
 @Repository
 public interface StayRepository extends JpaRepository<Stay, Long> {
+
     List<Stay> findByHost(User user);
 
     Stay findByIdAndHost(Long id, User host);
